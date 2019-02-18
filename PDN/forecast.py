76c1e5ef30abc691d7d1df0f14cafc5ee8d2cacc
Alexis Saul Alvarez Soto <alexisalvarez@grupovidanta.com>
2019-02-18 14:56:10 -0600

    Forecasted

--- PDN/forecast.py
+++ PDN/forecast.py
@@ -1,6 +1,17 @@
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import Ridge
+import matplotlib.pyplot as plt
 
 df = pd.read_excel('data.xlsx')
 df['target'] = np.cumsum(df['Producción'])
+
+X = np.array([[i+1] for i in range(df.shape[0])])
+model = Ridge(fit_intercept=True, alpha=0.0, random_state=0, normalize=True)
+model.fit(X,df['target'])
+
+X = np.array([[i+1] for i in range(29)])
+plt.plot(df['target'])
+plt.plot(model.predict(X),linestyle=':')
+plt.show()
 
